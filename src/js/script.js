3368b7b3cb581f54c4b9291a81579e27dc52fc4c
xandesi <alexsinderski@yahoo.com>
2024-05-27 15:22:02 -0300

    27/05/2024 15:21 i got your willis right here

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+for (const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+
+//quando clicar em um tem do menu, esconder o menu
+
+const links = document.querySelectorAll('nav ul li a')
+for(const link of links){
+    link.addEventListener('click',function(){
+nav.classList.remove('show')
+})
+}
+
+// Carrosel Swiper
+const swiper = new Swiper('.swiper-container',{
+    slidesPerView: 1,
+    pagination:{
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints:{
+        767:{
+            slidePerView:2,
+            setWrapperSize: true
+        }
+    }
+})    
